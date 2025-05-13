ae022f83865335a821a6b8c5331d248f24eebe8f
Nathaniel Bassett <bassettn1@cardiff.ac.uk>
2025-05-13 11:46:01 +0100

    updated styling

--- portfolio-site/src/pages/index.tsx
+++ portfolio-site/src/pages/index.tsx
@@ -7,14 +7,27 @@ import { TabProvider } from '@/contexts/TabContext';
 export default function Home() {
   return (
     <TabProvider>
-      <div className="min-h-screen flex flex-col">
-        <div className="flex flex-1">
+      <div className="h-screen flex flex-col bg-[#1E1E1E] text-[#D9D9D9] font-mono">
+        {/* Header (optional) */}
+        <div className="h-8 bg-[#1A1A40] flex items-center px-4">
+          <div className="text-sm text-white">Nathaniel's Portfolio</div>
+        </div>
+
+        {/* Main workspace area */}
+        <div className="flex flex-1 overflow-hidden">
+          {/* Sidebar (Explorer) */}
           <Explorer />
-          <div className="flex flex-col flex-1">
+
+          {/* Main Content Area */}
+          <div className="flex flex-col flex-1 overflow-hidden">
+            {/* Tabs */}
             <Tabs />
+            {/* Editor */}
             <EditorWindow />
           </div>
         </div>
+
+        {/* Terminal footer */}
         <Terminal />
       </div>
     </TabProvider>
